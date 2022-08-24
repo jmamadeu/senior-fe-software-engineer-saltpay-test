f0c0ec8a9b0108378bfe96cb4ec82a4d6a749d71
jmamadeu <jmamadeu2000@gmail.com>
2022-08-24 21:12:05 +0100

    chore: fix cnetered content

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import { CountriesListGrid } from "./components/countries-list-grid/countries-list-grid";
 import { Header } from "./components/header/header";
 import { useApiFetchAllCountries } from "./services/api/use-api-fetch-all-countries";
 
 function App() {
-  const { data: allCountries } = useApiFetchAllCountries()
+  const { data: allCountries } = useApiFetchAllCountries();
 
-  console.log(allCountries)
+  console.log(allCountries);
   return (
     <div>
       <Header />
 
-      <div>
-        
+      <div style={{
+        maxWidth: "1400px",
+        margin: "0 auto",
+        marginTop: 20
+      }}>
+        {allCountries && <CountriesListGrid countries={allCountries} />}
       </div>
     </div>
   );
