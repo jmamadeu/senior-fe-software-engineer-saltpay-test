bd5f879b2c9260a374825b763c10b9b718c97da8
jmamadeu <jmamadeu2000@gmail.com>
2022-08-25 00:18:20 +0100

    chore: add filter by region

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,56 @@ import { Header } from "./components/header/header";
 import { SearchInput } from "./components/search-input/search-input";
 import { useApiFetchAllCountries } from "./services/api/use-api-fetch-all-countries";
 
-function App() {
-  const [search, setSearch] = useState("")
-  const { data: allCountries } = useApiFetchAllCountries({countryToSearch: search});
+import styles from "./App.module.css";
 
+import Select from "react-select";
+
+const OPTIONS = [
+  { value: "All", label: "All" },
+  { value: "Europe", label: "Europe" },
+  { value: "Americas", label: "Americas" },
+  { value: "Asia", label: "Asia" },
+  { value: "Oceania", label: "Oceania" },
+  { value: "Africa", label: "Africa" },
+];
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [region, setRegion] = useState<{ value?: string; label?: string }>(
+    OPTIONS[0],
+  );
+
+  const { data: allCountries } = useApiFetchAllCountries({
+    countryToSearch: search,
+  });
+
+  const filteredCountries =
+    region.value === "All"
+      ? allCountries
+      : allCountries?.filter(
+          (country) =>
+            country.region.toLowerCase() === region.value?.toLocaleLowerCase(),
+        );
 
   return (
     <div>
       <Header />
 
-      <div
-        style={{
-          maxWidth: "1400px",
-          margin: "20px auto",
-          padding: "0 20px"
-          
-        }}
-      >
-        <div style={{
-          marginBottom: "20px"
-        }}>
-          <SearchInput onChange={value => setSearch(value)} />
+      <div className={styles.mainContainer}>
+        <div
+          className={styles.subHeader}
+        >
+          <SearchInput onChange={(value) => setSearch(value)} />
+          <Select
+            options={OPTIONS}
+            value={region}
+            onChange={(e) => setRegion({ value: e?.value, label: e?.label })}
+          />
         </div>
 
-        {allCountries && <CountriesListGrid countries={allCountries} />}
+        {filteredCountries && (
+          <CountriesListGrid countries={filteredCountries} />
+        )}
       </div>
     </div>
   );
